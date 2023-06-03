bfe3c847f0c4370634aa93fde2f78629d129fe31
GODA PRABHAKARA RAO <128283295+PrabhaKar-Rao@users.noreply.github.com>
2023-06-03 18:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 
 function palindrome(str){
 
+	str = str.toLowerCase();
+	let string = str.split(" ").join('')
+	let arr=string.split('');
+	let i=0;j=arr.length-1;
+
+	while(i<j){
+		if(arr[i] !== arr[j]){
+			return (str+" is not a palindrome")
+		}
+	}
+	return (str+" is a plaindrome");
 }
 module.exports = palindrome
